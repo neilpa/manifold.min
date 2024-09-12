45de4b6e5d25f1e2148ef3be4fe06dc9bf07eb5b
Neil Pankey <git@neilpa.me>
2024-09-12 06:52:15 -0700

    esbuild doesn't work by default

---
+++ main.js
@@ -0,0 +1,11 @@
+import Module from 'manifold-3d';
+
+const wasm = await Module();
+wasm.setup();
+const { Manifold } = wasm;
+
+const box = Manifold.cube([100, 100, 100], true);
+const ball = Manifold.sphere(60, 100);
+const result = box.subtract(ball);
+
+console.log(result.getMesh());
